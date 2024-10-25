cdff80f59afa079c6b1f3527de1d940265a41b60
SAHIL <98725819+Chaugulesahil04@users.noreply.github.com>
2024-10-25 21:20:03 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";  // Ensure this path is correct and the file exists
+import EditingPage from "./components/EditingPage/EditingPage";
+import Home from "./components/HomePage/Home";
+import Category from "./components/CategoryPage/Category";
+import Btemp1 from "./components/categories/Btemp1/Btemp1";
+import Wtemp1 from "./components/categories/wedding/Wtemp1/Wtemp1";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Category />} />
+        <Route path="/editing" element={<EditingPage />} />
+        <Route path="/Wtemp1" element={<Wtemp1 />} />
+        <Route path="/Btemp1" element={<Btemp1 />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
